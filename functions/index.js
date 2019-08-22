62f46bd452f11fd577786c69d6b6c29c9112b5ae
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-08-22 23:55:30 +0200

    webPushNotification - continue

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,52 @@
-const functions = require('firebase-functions')
-const {Storage} = require('@google-cloud/storage')
-const spawn = require('child-process-promise').spawn
+const functions = require('firebase-functions');
+const {Storage} = require('@google-cloud/storage');
+const spawn = require('child-process-promise').spawn;
+const admin = require('firebase-admin');
 
-const projectId = "iot-project-249706"
+admin.initializeApp(functions.config().firebase);
+
+const projectId = "iot-project-249706";
 const gcs = new Storage({
   projectId: projectId,
 });
 
-exports.generateThumbnail = functions.storage.object()
-      .onFinalize((object, context) => {
-        const filePath = object.name
-        const fileName = filePath.split('/').pop()
-        const fileBucket = object.bucket
-        const bucket = gcs.bucket(fileBucket)
-        const tempFilePath = '/tmp/${fileName}'
+exports.generateThumbnail = functions.storage.object().onFinalize((object, context) => {
+        const filePath = object.name;
+        const fileName = filePath.split('/').pop();
+        const fileBucket = object.bucket;
+        const bucket = gcs.bucket(fileBucket);
+        const tempFilePath = '/tmp/${fileName}';
+
+        const title = "SmartDoorBell";
+        const body = "Someone is at the door!";
+
+        admin.messaging.WebPushNotification(title, body);
 
         if(fileName.startsWith('thumb_')){
-          console.log("Already a thumbnail.")
-          return
+          console.log("Already a thumbnail.");
+          return;
         }
 
         if(!object.contentType.startsWith('image/')){
-          console.log("This is not an image!")
-          return
+          console.log("This is not an image!");
+          return;
         }
 
         if(object.resourceState === 'not_exists'){
-          console.log("Picture was deleted.")
-          return
+          console.log("Picture was deleted.");
+          return;
         }
 
         return bucket.file(filePath).download({
           destination: tempFilePath
         })
         .then(() => {
-          console.log("Image downloaded localy to", tempFilePath)
-          return spawn('convert', [tempFilePath, '-thumbnail', '200x200>',
-              tempFilePath])
+          console.log("Image downloaded localy to", tempFilePath);
+          return spawn('convert', [tempFilePath, '-thumbnail', '200x200', tempFilePath]);
         })
         .then(() => {
-          console.log("Thumbnail created.")
-          const thumbFilePath = filePath.replace(/(\/)?([^\/]*)$/,
-              '$1thumb_$2')
+          console.log("Thumbnail created.");
+          const thumbFilePath = filePath.replace(/(\/)?([^\/]*)$/, '$1thumb_$2');
 
           return bucket.upload(tempFilePath, {
             destination: thumbFilePath
